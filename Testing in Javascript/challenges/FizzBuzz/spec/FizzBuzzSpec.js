f95baea4ac98679684a221c76023eab84461f653
Csilla Pap <csill.pap@gmail.com>
2017-01-17 00:04:39 +0000

    corrected the specs file in FizzBuzz challenge

--- Testing in Javascript/challenges/FizzBuzz/spec/FizzBuzzSpec.js
+++ Testing in Javascript/challenges/FizzBuzz/spec/FizzBuzzSpec.js
@@ -1,27 +1,30 @@
+
+	
+	
 describe ("FizzBuzz test", function() {
-	var number = 0; 
-	while (number < 100) {
-		FizzBuzz(number); //will execute the FizzBuzz function until the condition of the while loop is true
-		number++; //after executing the function it will increase the number by 1 until reaching 100
-		//console.log(FizzBuzz(number));
-	} 
+ 
 	describe('FizzBuzz', function() {
-			it("should call the alert function if the number given is undefined", function() {
-            spyOn(window, "alert");
-            FizzBuzz();
-            expect(window.alert).toHaveBeenCalledWith("Argument must be a number!");
+
+		function test_hundred_times(x) {
+
+			it('Test for number '+ x +'. Should return FizzBuzz if the number can be divided by 5 and 3', function() {
+				expect( FizzBuzz(x) ).toBe("FizzBuzz");
 			});
-			it('should return FizzBuzz if the number can be divided by 5 and 3', function() {
-				expect(FizzBuzz(15)).toBe("FizzBuzz");
+			it('Test for number '+ x +'. Should return Fizz if the number can be divided only by 3', function() {
+				expect( FizzBuzz(x) ).toBe("Fizz");
 			});
-			it('should return Fizz if the number can be divided by 3', function() {
-				expect(FizzBuzz(9)).toBe("Fizz");
+			it('Test for number '+ x +'. Should return Buzz if the number can be divided only by 5', function() {
+				expect( FizzBuzz(x) ).toBe("Buzz");
 			});
-			it('should return Buzz if the number can be divided by 5', function() {
-				expect(FizzBuzz(10)).toBe("Buzz");
+			it('Test for number '+ x +'. Should return number if the number can not be divided by 5 or by 3', function() {
+				expect( FizzBuzz(x) ).toBe(x);
 			});
-			it('should return the number itself if cant be divided by 3 or 5', function() {
-				expect(FizzBuzz(17)).toBe(17);
-			});
-		});
+		}
+
+		for(var x = 1; x <= 100; x++) {
+		    test_hundred_times(x);
+		}
+
 	});
+
+}); 
