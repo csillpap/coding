f8c2c7329d12a37bee346cc8120421c54fd5c1e0
Csilla Pap <csill.pap@gmail.com>
2017-04-09 21:49:10 +0100

    Amend function to fill the grid with letters

--- STREAM 2/boggle/boggle.py
+++ STREAM 2/boggle/boggle.py
@@ -1,9 +1,10 @@
+from string import ascii_uppercase
+from random import choice
+
 def check():
     return 1
 
 def make_grid(width, height):
-    return {}
-
-def make_grid(height, width):
-    return {(row, col): ' ' for row in range(height)
-                            for col in range(width)}
+    return {(row, col): choice(ascii_uppercase)
+                for row in range(height)
+                for col in range(width)}
