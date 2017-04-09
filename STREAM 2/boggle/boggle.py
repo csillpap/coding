a19265b754e1fb85e08b163cd0665baff86268c6
Csilla Pap <csill.pap@gmail.com>
2017-04-09 21:55:36 +0100

    Create the function for the neighbouring positions

--- STREAM 2/boggle/boggle.py
+++ STREAM 2/boggle/boggle.py
@@ -8,3 +8,8 @@ def make_grid(width, height):
     return {(row, col): choice(ascii_uppercase)
                 for row in range(height)
                 for col in range(width)}
+
+def neighbours_of_position((row, col)):
+    return [ (row - 1, col - 1), (row - 1, col), (row - 1, col + 1),
+             (row, col - 1),                     (row, col + 1),
+             (row + 1, col - 1), (row + 1, col), (row + 1, col + 1)]
