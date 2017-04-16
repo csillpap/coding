49d6805aa3a61378722afcdce1cb02c0d5a0ce74
Csilla Pap <csill.pap@gmail.com>
2017-04-16 13:09:05 +0100

    Create program to ask the questions and count the right answers

--- STREAM 2/File IO and Exceptions/quiz.py
+++ STREAM 2/File IO and Exceptions/quiz.py
@@ -2,3 +2,13 @@ def get_questions():
     with open('questions.txt') as f:
         lines = f.readlines()
     return [(lines[i], lines[i+1].strip()) for i in range(0, len(lines), 2)]
+
+
+questions = get_questions()
+score = 0
+total = len(questions)
+for question, answer in questions:
+    guess = raw_input(question)
+    if guess == answer:
+        score += 1
+print 'You got %s out of %s questions right' % (score, total)
