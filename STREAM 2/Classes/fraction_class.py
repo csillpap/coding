befe9e232faeabc1716295edd7afc964a7c104c3
Csilla Pap <csill.pap@gmail.com>
2017-04-17 19:27:23 +0100

    Add method for representation

--- STREAM 2/Classes/fraction_class.py
+++ STREAM 2/Classes/fraction_class.py
@@ -2,3 +2,6 @@ class Fraction:
     def __init__(self, numerator, denominator):
         self.num = numerator
         self.den = denominator
+
+    def __repr__(self):
+        return '%s/%s' % (self.num, self.den)
