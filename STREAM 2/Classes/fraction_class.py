169928805fb637c238c113a3864d1494ae05b249
Csilla Pap <csill.pap@gmail.com>
2017-04-17 19:33:12 +0100

    Add method for adding numbers together

--- STREAM 2/Classes/fraction_class.py
+++ STREAM 2/Classes/fraction_class.py
@@ -5,3 +5,8 @@ class Fraction:
 
     def __repr__(self):
         return '%s/%s' % (self.num, self.den)
+
+    def __add__(self, other):
+        num = self.num * other.den + self.den * other.num
+        den = self.den * other.den
+        return Fraction(num, den)
