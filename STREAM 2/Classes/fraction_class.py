38b59c1163e3c604d28254868e44ce3f9cbc8e8b
Csilla Pap <csill.pap@gmail.com>
2017-04-17 19:33:53 +0100

    Add multiply, subtract and divide method

--- STREAM 2/Classes/fraction_class.py
+++ STREAM 2/Classes/fraction_class.py
@@ -10,3 +10,14 @@ class Fraction:
         num = self.num * other.den + self.den * other.num
         den = self.den * other.den
         return Fraction(num, den)
+
+    def __sub__(self, other):
+        num = self.num * other.den - self.den * other.num
+        den = self.den * other.den
+        return Fraction(num, den)
+
+    def __mul__(self, other):
+        return Fraction(self.num * other.num, self.den * other.den)
+
+    def __div__(self, other):
+        return Fraction(self.num * other.den, self.den * other.num)
