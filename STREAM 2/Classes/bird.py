8aba852f3db7eab038495b3310dd7b4d71cbcc41
Csilla Pap <csill.pap@gmail.com>
2017-04-17 12:20:10 +0100

    Add specialised behaviour to the Parrot subclass

--- STREAM 2/Classes/bird.py
+++ STREAM 2/Classes/bird.py
@@ -9,6 +9,14 @@ class Bird:
 class Parrot(Bird):
     def __init__(self):
         Bird.__init__(self, 'Parrot', 'Kah!')
+        self.learned_phrases = set()
+
+    def learn_phrase(self, phrase):
+        self.learned_phrases.add(phrase)
+
+    def do_call(self):
+        Bird.do_call(self)
+        print '\n'.join(self.learned_phrases)
 
 
 parrot = Parrot()
