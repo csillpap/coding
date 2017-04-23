a7815a7b92d7e07c268f3aff072b4139fc574c5c
Csilla Pap <csill.pap@gmail.com>
2017-04-23 12:08:52 +0100

    Add function to find out what tables do we have in our database

--- STREAM 2/Using Python with MySql - part1/database/mysql.py
+++ STREAM 2/Using Python with MySql - part1/database/mysql.py
@@ -36,3 +36,18 @@ class MySQLDatabase(object):
         if hasattr(self, 'db'):
             self.db.close()
             print "MySQL Connection Closed"
+
+    def get_available_tables(self):
+        """
+        This method will allow us to what
+        tables are available to us when we're
+        running our queries
+        """
+        cursor = self.db.cursor()
+        cursor.execute("SHOW TABLES;")
+
+        self.tables = cursor.fetchall()
+
+        cursor.close()
+
+        return self.tables
