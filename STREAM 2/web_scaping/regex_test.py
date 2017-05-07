8f7a686cf1980741d4136380dd8658817b4ee4df
Csilla Pap <csill.pap@gmail.com>
2017-05-07 15:08:13 +0100

    Modify re.sub() to replace all occurrences of the tags themselves, not everything between the first < and last > as it happened using re.sub() before

--- STREAM 2/web_scaping/regex_test.py
+++ STREAM 2/web_scaping/regex_test.py
@@ -32,3 +32,11 @@ a_string = "Everything we do is <replaced> if it is indeed inside <tags>."
 # Substitute the tags with 'coming up roses' using the re.sub() method
 a_string = re.sub("<.*>", "coming up roses", a_string)
 print a_string
+
+
+another_string = "Everything we do is <replaced> if it is indeed inside <tags>."
+
+# Make sure that both tags are replaced by using the ? too tell
+# re.sub() to stop after first match of '>'
+another_string = re.sub("<.*?>", "coming up roses", another_string)
+print another_string
